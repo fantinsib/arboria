042447c5adbde0bb2ba20fd70eebd4b21e9c78d9
Fantin Sibony <fantinsibony@gmail.com>
2025-12-30 09:37:23 +0100

    Add Node class

---
+++ src/node/node.cpp
@@ -0,0 +1,24 @@
+#include "myforest/node.h"
+#include <random>
+
+namespace myforest {
+
+Node::Node():
+    feature_index(-1),
+    threshold(0.0f),
+    predicted_class(-1)
+
+{}
+
+int Node::return_feature_index() const{
+
+    return this->feature_index;
+
+}
+
+float Node::return_threshold() const{
+
+    return this->threshold;
+}
+
+}
---
+++ src/node/node.h
@@ -0,0 +1,35 @@
+#ifndef NODE_H
+#define NODE_H
+#include <memory>
+#include <vector>
+
+namespace myforest{
+class Node
+{
+public:
+    /**
+     * @brief Creates a new Node 
+     * 
+     * Node stores key informations for a split :
+     * feature_index : the column index on which the split is made (default value -1)
+     * threshold : the threshold that splits the targeted feature (default value 0.0)
+     * predicted_class : the majority class received by the node (default value -1)
+     * left_child and right_child : pointers to the next nodes
+     */
+    Node();
+    int feature_index;
+    float threshold;
+    int predicted_class;
+
+    bool is_leaf = true;
+
+    int return_feature_index() const;
+    float return_threshold() const;
+
+    std::vector<int> rows;
+
+    std::unique_ptr<Node> left_child;
+    std::unique_ptr<Node> right_child;
+};
+}
+#endif // NODE_H
